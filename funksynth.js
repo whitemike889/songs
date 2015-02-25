1c15cc1d1f80cc0e02e9d0c76544b9987b014484
James Halliday <mail@substack.net>
2015-02-25 11:04:35 -0800

    extra funk

--- funksynth.js
+++ funksynth.js
@@ -9,7 +9,18 @@ return function (t) {
   var p = funk[Math.floor(t*4)%funk.length];
   var m = melody[Math.floor(t)%melody.length];
   var n = melody[Math.floor(t/4)%melody.length];
+  var x = melody[Math.floor(t*8)%melody.length];
   return 0
+    + saw(x * 16 + saw(x*m*10)/4) * (1-sq(16*(m)))/2
+        * (sq(1/16) > 0 ? 0 : 1) * 0.1
+    + (0
+      + saw(x * 2 + saw(x*x)/2) * (1-saw(4*x))/2 * (1+sq(2+m))/2 * 0.2
+      + sq(
+        + (tri(1800) * 5 + tri(9000) * 4) * sq(3) * sq(4)
+      ) * sq_(tri(1/4),400+(t%2>1?tri(x*2+100):tri(8))) * 0.2
+      + clip(d(t % (1/p[0]/4)))
+    ) * (sq(1/16)>0?1:0)
+    
     + clip(d(t % (1/p[0]) * p[1]) * sq(p[2]*100)) * 0.3
     + saw(m) * (1-saw(4))/2 * (1+sq(2))/2 * 0.3
     + saw(m + 1/8) * (1-saw(4))/2 * (1+sq(2))/2 * 0.3
@@ -26,9 +37,11 @@ return function (t) {
     ) * (1-saw(4)) / 2 * (1+sq(1/4))/2 * 0.3
   ;
   function tri (x) { return Math.abs(1 - t % (1/x) * x * 2) * 2 - 1}
+  function tri_ (t,x) { return Math.abs(1 - t % (1/x) * x * 2) * 2 - 1}
   function saw (x) { return t%(1/x)*x*2-1 }
   function sin (x) { return Math.sin(2 * Math.PI * t * x) }
   function sq (x) { return t*x % 1 < 0.5 ? -1 : 1 }
+  function sq_ (t,x) { return t*x % 1 < 0.5 ? -1 : 1 }
   function clip (x) {
     if (x === Infinity || x === -Infinity || isNaN(x)) return 0;
     return Math.min(1, Math.max(-1, x));
